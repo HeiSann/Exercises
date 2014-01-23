8595037221c1172a913f4ddab457da02fe6c1470
Magne Normann <magnealvnor@gmail.com>
2014-01-23 11:08:20 +0100

    Sanntidsoving 3, TCP and UDP socket programming

---
+++ oving3TCP.go
@@ -0,0 +1,69 @@
+package main
+import(
+	"net"
+	"fmt"
+	"runtime"
+)
+const MY_IP= "129.241.187.159"
+const MY_PORT = "20011"
+const TARGET_PORT = "33546"
+const TARGET_IP = "129.241.187.161"
+
+func connectTo(ipAdr string, port string)
+{
+
+	serverAddr, err := net.ResolveTCPAddr("tcp",ipAdr+":"+port)
+	con, err := net.DialTCP("tcp", nil, serverAddr);
+	
+	if err != nil 
+	{
+		fmt.Println("fail")
+	}
+	cd := make([]byte,1024)
+	con.Read(cd)
+	fmt.Printf("%s",cd)	
+	stop :=0
+	
+	msg:= "Connect to: "+MY_IP+":"+MY_PORT+"\x00"
+	con.Write([]byte(msg))	
+	
+	for stop !=-1
+	{
+		input := ""
+		fmt.Scanf("%s",&input)
+		if input=="stop"
+		{
+			stop=-1
+		}
+		con.Write([]byte(input+"\x00"))
+		con.Read(cd)
+		fmt.Printf("%s",cd)
+	}
+}
+
+func ListenerCon(port string)
+	{
+	psock, err := net.Listen("tcp", ":"+port)
+	if err != nil { return }
+	buf := make([]byte,1024)
+ 
+  	for 
+  	{
+    		conn, err := psock.Accept()
+    		if err != nil { return }
+    		conn.Read(buf)
+		fmt.Printf("%s",buf)
+	 }		
+}	
+
+func main(){
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	
+	go connectTo(TARGET_IP,TARGET_PORT)
+	go ListenerCon(MY_PORT)	
+	
+	
+	for true
+	{
+	}
+}
