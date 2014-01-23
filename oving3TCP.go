0b2722df2d554da791490c89216b73808f14866d
Magne Normann <magnealvnor@gmail.com>
2014-01-23 12:11:02 +0100

    Fixed function format

--- oving3TCP.go
+++ oving3TCP.go
@@ -15,8 +15,7 @@ func connectTo(ipAdr string, port string)
 	serverAddr, err := net.ResolveTCPAddr("tcp",ipAdr+":"+port)
 	con, err := net.DialTCP("tcp", nil, serverAddr);
 	
-	if err != nil 
-	{
+	if err != nil{
 		fmt.Println("fail")
 	}
 	cd := make([]byte,1024)
@@ -27,8 +26,7 @@ func connectTo(ipAdr string, port string)
 	msg:= "Connect to: "+MY_IP+":"+MY_PORT+"\x00"
 	con.Write([]byte(msg))	
 	
-	for stop !=-1
-	{
+	for stop !=-1{
 		input := ""
 		fmt.Scanf("%s",&input)
 		if input=="stop"
@@ -41,14 +39,12 @@ func connectTo(ipAdr string, port string)
 	}
 }
 
-func ListenerCon(port string)
-	{
+func ListenerCon(port string){
 	psock, err := net.Listen("tcp", ":"+port)
 	if err != nil { return }
 	buf := make([]byte,1024)
  
-  	for 
-  	{
+  	for{
     		conn, err := psock.Accept()
     		if err != nil { return }
     		conn.Read(buf)
